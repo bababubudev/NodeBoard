57028e88d3a214e5406d76b0a6a6e76b5e6e2b12
bababubudev <buttblaster01@gmail.com>
2022-12-22 15:43:07 +0200

    Added server and html
    TODO: Add css

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 import * as mods from "./modules.js"
+import http from "http"
+
+const server = http.createServer(on_request);
+server.listen(mods.port_number, "localhost", on_listen);
+
+function on_request(req, res)
+{
+    console.log("Request of type " + req.method + " was made!");
+    res.setHeader("Content-Type", "text/html");
+
+    let path = "./views/";
+
+    switch (req.url)
+    {
+        case "/":
+            path += "base.html";
+            break;
+        case "/inbox":
+            path += "customarea.html";
+            break;
+        default:
+            path += "404.html";
+            res.statusCode = 404;
+            break;
+    }
+
+    mods.read_write(path, res);
+}
+
+function on_listen()
+{
+    console.log("Listening on port " + mods.port_number);
+}
--- modules.js
+++ modules.js
@@ -1,12 +1,28 @@
 import * as Sutukka from "dotenv"
+import fs from "fs"
 Sutukka.config();
 
 const keys = {
     naam: process.env.NAAM
 }
 
-const user = {
-    name: "Prabesh",
+const port_number = 3000;
+
+export function read_write(url, response)
+{
+    fs.readFile(url, (err, data) =>
+    {
+        if (err)
+        {
+            console.log(err);
+            response.end();
+        }
+        else
+        {
+            response.write(data);
+            response.end();
+        }
+    });
 }
 
-export { keys, user }
+export { keys, port_number };

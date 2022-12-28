804048c4851cd1ee1dd3d42a07afa3cd007e038a
bababubudev <prwshshrm@gmail.com>
2022-12-28 12:58:51 +0200

    Everything works logically now
    I think. TODO: Use buttons to somehow make shit work!

--- modules.js
+++ modules.js
@@ -21,9 +21,31 @@ const object_session = {
     cookie: { secure: false }
 }
 
+const object_default = {
+    _id: null,
+    linker: "User",
+    text: "",
+    createdAt: null,
+    updatedAt: null
+}
+
 const port_number = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export { keys, port_number, __dirname, object_blueprint, object_session };
+export function log(_status)
+{
+    console.log(_status);
+}
+
+export function stringed(_data)
+{
+    return JSON.stringify(_data);
+}
+
+export
+{
+    keys, port_number, __dirname,
+    object_blueprint, object_session, object_default
+};
